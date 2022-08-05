0c719408490d43d9e7db58879343aa7488fae5bb
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-08-05 15:03:56 -0300

    feat: add new route and packages needed
    This route is used to create a new user.
    If the email you are trying to register already exists return 409.
    If the body is sent incorrectly return 422.
    If all goes rigth return 201.

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
 
+import { authMiddlewares } from '../middlewares/index.js';
+import { usersController } from '../controllers/index.js';
+
 const router = express.Router();
+
+// Authentication routes
+router.post(
+  '/signup',
+  authMiddlewares.checkSignup,
+  usersController.registerUser
+);
 
 export default router;
