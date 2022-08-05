ffd4f282186d85bdd1360f09b7f08270afdca0b0
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-08-05 14:40:57 -0300

    feat: add function to models
    This function creates a new entry on users.

--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -1,8 +1,18 @@
 import joi from 'joi';
 
+import { connection } from './index.js';
+
 export const signupSchema = joi.object({
   name: joi.string().trim().required(),
   email: joi.string().trim().email().required(),
   password: joi.string().trim().required(),
   confirmPassword: joi.string().trim().equal(joi.ref('password')),
 });
+
+export const createUser = async (user) => {
+  const { name, email, password } = user;
+  await connection.query(
+    'INSERT INTO users (name, email, password) VALUES ($1, $2, $3)',
+    [name, email, password]
+  );
+};
