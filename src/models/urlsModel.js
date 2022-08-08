52bbe452737512c88e631cd68396cd4c0623c868
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-08-08 12:13:56 -0300

    feat: add new function to create new short url

--- src/models/urlsModel.js
+++ src/models/urlsModel.js
@@ -1,5 +1,14 @@
 import joi from 'joi';
+import { connection } from './index.js';
 
 export const urlSchema = joi.object({
   url: joi.string().trim().uri().required(),
 });
+
+export const createLink = async (linkObject) => {
+  const { shortUrl, url, ownerId } = linkObject;
+  await connection.query(
+    'INSERT INTO links ("shortUrl", "url", "ownderId") VALUES ($1, $2, $3)',
+    [shortUrl, url, ownerId]
+  );
+};
