38b5b5b409f07fa95dd395480df5437d7f1991c5
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-08-08 13:50:34 -0300

    feat: add new middleware to check existance and ownership
    This middleware searches by id the link received by params.
    It returns 404 if the id doesn't exist.
    If it exists, see if the owner of the link and the owner passed by token are the same.
    If it is, pass the link along.
    If not, returns 401.

--- src/middlewares/urlsMiddlewares.js
+++ src/middlewares/urlsMiddlewares.js
@@ -10,3 +10,27 @@ export const checkUrl = (req, res, next) => {
   next();
   return true;
 };
+
+export const checkOwner = async (req, res, next) => {
+  const { ownerId } = res.locals;
+  const { urlId } = req.params;
+  try {
+    const link = await urlsModel.getLinkById(urlId);
+    if (!link) {
+      return res.status(404).send('Esse link não existe.');
+    }
+
+    if (ownerId !== link.ownerId) {
+      return res
+        .status(401)
+        .send('Você não pode apagar um link que não é seu.');
+    }
+
+    res.locals.link = link;
+    next();
+    return true;
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Algo deu errado ao buscar pelo seu link.');
+  }
+};
